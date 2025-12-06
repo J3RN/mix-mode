411c8947feb271fa8add43a6bd4e35ad7bce67d4
Jonathan Arnett <j3rn@j3rn.com>
2025-12-06 12:22:37 -0500

    Change mix-project-root to accept property list options

--- mix.el
+++ mix.el
@@ -141,11 +141,13 @@ Use `mix--all-available-tasks` to fetch formatted and filetered tasks."
     (or (mix-umbrella-root (mix--up-directory project-dir))
         project-dir)))
 
-(defun mix-project-root (&optional prefer-umbrella)
+(defun mix-project-root (&rest opts)
   "Find the root of the current Elixir project."
-  (if prefer-umbrella
-      (mix-umbrella-root default-directory)
-    (locate-dominating-file default-directory "mix.exs")))
+  (let ((prefer-umbrella (plist-get opts :prefer-umbrella))
+        (directory (or (plist-get opts :directory) default-directory)))
+    (if prefer-umbrella
+        (mix-umbrella-root directory)
+      (locate-dominating-file directory "mix.exs"))))
 
 
 
@@ -153,12 +155,12 @@ Use `mix--all-available-tasks` to fetch formatted and filetered tasks."
 
 (defun mix-project-run-shell ()
   (interactive)
-  (let ((default-directory (mix-project-root mix-prefer-umbrella)))
+  (let ((default-directory (mix-project-root :prefer-umbrella mix-prefer-umbrella)))
     (shell)))
 
 (defun mix-project-compile ()
   (interactive)
-  (let ((default-directory (mix-project-root mix-prefer-umbrella))
+  (let ((default-directory (mix-project-root :prefer-umbrella mix-prefer-umbrella))
         (cmd (if current-prefix-arg
                  (read-from-minibuffer "Command: " "mix compile")
                "mix compile")))
@@ -168,7 +170,7 @@ Use `mix--all-available-tasks` to fetch formatted and filetered tasks."
 
 (defun mix-execute-task ()
   (interactive)
-  (let* ((default-directory (mix-project-root mix-prefer-umbrella))
+  (let* ((default-directory (mix-project-root :prefer-umbrella mix-prefer-umbrella))
          (task (mix--remove-task-comment
                 (completing-read "Select mix task: " (mix--all-available-tasks default-directory) nil nil nil 'mix-selected-task)))
          (final-task (if current-prefix-arg
